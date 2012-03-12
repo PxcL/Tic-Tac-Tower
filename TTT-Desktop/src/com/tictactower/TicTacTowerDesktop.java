0df7bd929ae131fe61689438cf8645575ff438d5
unknown <Eivind@Eivind2-PC.(none)>
2012-03-12 16:22:58 +0100

    Changed background color.

--- TTT-Desktop/src/com/tictactower/TicTacTowerDesktop.java
+++ TTT-Desktop/src/com/tictactower/TicTacTowerDesktop.java
@@ -27,7 +27,7 @@ public class TicTacTowerDesktop implements ApplicationListener {
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 	}
