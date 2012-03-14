c64576f448b8ef01b02c3e5cc015bfea724367eb
dalum <goran.dalum@gmail.com>
2012-03-14 15:19:07 +0100

    Opprettet pakke og klasser for Player

---
+++ TTT-Desktop/src/com/tictactower/player/Player.java
@@ -0,0 +1,17 @@
+package com.tictactower.player;
+
+import com.tictactower.Game;
+
+public abstract class Player {
+	
+	protected int empCount;
+	protected int newTowerCount;
+	protected int destroyTowerCount;
+	protected int lastSkillCount;
+
+	public void setMark(int x, int y, Player player) {
+		Game.getInstance().getGameboard().setMark(x, y, player);
+	}
+	
+	
+}
---
+++ TTT-Desktop/src/com/tictactower/player/Player2.java
@@ -0,0 +1,9 @@
+package com.tictactower.player;
+
+public class Player2 extends Player {
+
+	public void setMark(int x, int y) {
+		super.setMark(x, y, this);
+	}
+	
+}
