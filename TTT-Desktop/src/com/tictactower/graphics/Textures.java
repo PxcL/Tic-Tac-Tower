4d86501486650a81c27260eb226bbb4d7f9ebdfc
Eivind Karlsen <eivind_gmlmosseveg@hotmail.com>
2012-04-19 12:17:05 +0200

    endret navn paa texture-filer

--- TTT-Desktop/src/com/tictactower/graphics/Textures.java
+++ TTT-Desktop/src/com/tictactower/graphics/Textures.java
@@ -13,8 +13,8 @@ public class Textures {
 	public final static Texture MARK_P2_ACTIVE = new Texture("P2_ACTIVE.png");
 	
 	public final static Texture BUTTON_END_TURN = new Texture("txtBox_75black.png");
-	public final static Texture BUTTON_SILENCE_ACTIVE = new Texture("sile_active.png");
-	public final static Texture BUTTON_SILENCE_DEACTIVE = new Texture("sile_deactive.png");
+	public final static Texture BUTTON_SILENCE_ACTIVE = new Texture("emp_active.png");
+	public final static Texture BUTTON_SILENCE_DEACTIVE = new Texture("emp_deactive.png");
 	public final static Texture BUTTON_SHOOT_ACTIVE = new Texture("shoot_active.png");
 	public final static Texture BUTTON_SHOOT_DEACTIVE = new Texture("shoot_deactive.png");
 	public final static Texture BUTTON_BUILD_ACTIVE = new Texture("build_active.png");
